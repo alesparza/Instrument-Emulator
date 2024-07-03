05867dc124d8ad6c4960282bb3bc7c1c47ceb4e4
Antonio Esparza <aesparza@seas.upenn.edu>
2024-07-03 10:35:13 -0400

    feat: create new start panel window

---
+++ src/main/java/com/github/alesparza/emulator/gui/StartPanel.java
@@ -0,0 +1,17 @@
+package com.github.alesparza.emulator.gui;
+
+import javax.swing.*;
+
+public class StartPanel extends JFrame {
+
+
+  private JPanel contentPane;
+
+  public StartPanel() {
+    setTitle("Emulator");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setContentPane(contentPane);
+
+    pack();
+  }
+}
