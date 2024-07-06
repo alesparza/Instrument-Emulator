b549add618409684e4d92f5dc2bfaf047ecec62c
Antonio Esparza <aesparza@seas.upenn.edu>
2024-07-05 22:41:50 -0400

    fix: print formatted string correctly

--- src/main/java/com/github/alesparza/astm/ASCIIFormatter.java
+++ src/main/java/com/github/alesparza/astm/ASCIIFormatter.java
@@ -1,39 +1,50 @@
 package com.github.alesparza.astm;
 
+/**
+ * Formats ASCII Strings into a human-readable format.
+ */
 public class ASCIIFormatter {
 
-public enum AsciiString {
-  NUL((byte) 0x00, "<NUL>"),
-  SOH((byte) 0x01, "<SOH>"),
-  STX((byte) 0x02, "<STX>"),
-  ETX((byte) 0x03, "<ETX>"),
-  EOT((byte) 0x04, "<EOT>"),
-  ENQ((byte) 0x05, "<ENQ>"),
-  ACK((byte) 0x06, "<ACK>"),
-  TAB((byte) 0x09, "<TAB>"),
-  LF((byte) 0x0a, "<LF>"),
-  VT((byte) 0x0b, "<VT>"),
-  CR((byte) 0x0d, "<CR>"),
-  NAK((byte) 0x15, "<NAK>"),
-  ;
+  /**
+   * Enum for different control characters.
+   */
+  public enum AsciiControlFormat {
+    NUL((byte) 0x00, "\0", "<NUL>"),
+    SOH((byte) 0x01, "\1", "<SOH>"),
+    STX((byte) 0x02, "\2", "<STX>"),
+    ETX((byte) 0x03, "\3", "<ETX>"),
+    EOT((byte) 0x04, "\4", "<EOT>"),
+    ENQ((byte) 0x05, "\5" ,"<ENQ>"),
+    ACK((byte) 0x06, "\6", "<ACK>"),
+    TAB((byte) 0x09, "\t", "<TAB>"),
+    LF((byte) 0x0a, "\n", "<LF>"),
+    VT((byte) 0x0b, "\13", "<VT>"),
+    CR((byte) 0x0d, "\r", "<CR>"),
+    NAK((byte) 0x15, "\25","<NAK>"),
+    ;
 
-  private final byte asciiByte;
-  private final String asciiFormat;
+    /**
+     * Byte value of control character.
+     */
+    private final byte asciiByte;
 
-  private byte getAsciiByte() {
-    return asciiByte;
+    /**
+     * Escape string for control character.
+     */
+    private final String asciiEscape;
+
+    /**
+     * Formatted string for control character.
+     */
+    private final String asciiFormat;
+
+
+    AsciiControlFormat(byte b, String e, String s) {
+      asciiByte = b;
+      asciiEscape = e;
+      asciiFormat = s;
+    }
   }
-
-  private String getAsciiFormat() {
-    return asciiFormat;
-  }
-
-  AsciiString(byte b, String s) {
-    asciiByte = b;
-    asciiFormat = s;
-  }
-}
-
 
   /**
    * Formats a byte array to a readable String.
@@ -42,10 +53,9 @@ public enum AsciiString {
    */
   public static String getFormattedString(byte[] bytes) {
     String ret = new String(bytes);
-    for (AsciiString format : AsciiString.values()) {
-      ret = ret.replaceAll(String.valueOf(format.asciiByte), format.asciiFormat);
+    for (AsciiControlFormat format : AsciiControlFormat.values()) {
+      ret = ret.replaceAll(format.asciiEscape, format.asciiFormat);
     }
     return ret;
   }
-
 }
