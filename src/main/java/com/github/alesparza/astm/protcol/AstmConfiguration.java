a92b115503b93ef44d832879e6959993a764dcf4
Antonio Esparza <aesparza@seas.upenn.edu>
2024-07-11 14:14:36 -0400

    fix: remove magic numbers and add setters

--- src/main/java/com/github/alesparza/astm/protcol/AstmConfiguration.java
+++ src/main/java/com/github/alesparza/astm/protcol/AstmConfiguration.java
@@ -23,6 +23,11 @@ public class AstmConfiguration {
   public static char DEFAULT_ESCAPE_DELIM = '~';
 
   /**
+   * Default size of ASTM message: 64000
+   */
+  public static int DEFAULT_DATA_LENGTH = 64000;
+
+  /**
    * Field delimiter used by this configuration.
    */
   char fieldDelimiter;
@@ -43,6 +48,11 @@ public class AstmConfiguration {
   char escapeDelimiter;
 
   /**
+   * Length of size of ASTM messages.
+   */
+  int dataLength;
+
+  /**
    * Constructs a new ASTM configuration using the default delimiters.
    */
   public AstmConfiguration() {
@@ -50,6 +60,7 @@ public class AstmConfiguration {
     repeatDelimiter = DEFAULT_REPEAT_DELIM;
     componentDelimiter = DEFAULT_COMPONENT_DELIM;
     escapeDelimiter = DEFAULT_ESCAPE_DELIM;
+    dataLength = DEFAULT_DATA_LENGTH;
   }
 
   /**
@@ -59,11 +70,12 @@ public class AstmConfiguration {
    * @param componentDelimiter component delimiter to use
    * @param escapeDelimiter escape delimiter to use
    */
-  public AstmConfiguration(char fieldDelimiter, char repeatDelimiter, char componentDelimiter, char escapeDelimiter) {
+  public AstmConfiguration(char fieldDelimiter, char repeatDelimiter, char componentDelimiter, char escapeDelimiter, int dataLength) {
     this.fieldDelimiter = fieldDelimiter;
     this.repeatDelimiter = repeatDelimiter;
     this.componentDelimiter = componentDelimiter;
     this.escapeDelimiter = escapeDelimiter;
+    this.dataLength = dataLength;
   }
 
   public char getFieldDelimiter() {
@@ -80,5 +92,29 @@ public class AstmConfiguration {
 
   public char getEscapeDelimiter() {
     return escapeDelimiter;
+  }
+
+  public int getDataLength() {
+    return dataLength;
+  }
+
+  public void setFieldDelimiter(char fieldDelimiter) {
+    this.fieldDelimiter = fieldDelimiter;
+  }
+
+  public void setRepeatDelimiter(char repeatDelimiter) {
+    this.repeatDelimiter = repeatDelimiter;
+  }
+
+  public void setComponentDelimiter(char componentDelimiter) {
+    this.componentDelimiter = componentDelimiter;
+  }
+
+  public void setEscapeDelimiter(char escapeDelimiter) {
+    this.escapeDelimiter = escapeDelimiter;
+  }
+
+  public void setDataLength(int dataLength) {
+    this.dataLength = dataLength;
   }
 }
